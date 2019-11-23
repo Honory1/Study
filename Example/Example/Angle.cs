d4546c75e03f94dbf3011b22daefb838ed5f3007
Honory1 <skorpork@gmail.com>
2019-11-24 01:28:08 +0400

    some change

---
+++ Example/Example/Angle.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example
+{
+    public class Angle
+    {
+        private double _ax;
+        private double _ay;
+        private double _bx;
+        private double _by;
+        private double _cx;
+        private double _cy;
+
+        //вычисление координат векторов
+        private double _abx => _bx - _ax;
+        private double _aby => _by - _ay;
+        private double _bcx => _cx - _bx;
+        private double _bcy => _cy - _by;
+
+        private double _angle //формула для вычисления угла между векторами
+        {
+            get => Math.Acos((_abx * _bcx + _aby * _bcy) / Math.Sqrt(_abx * _abx + _aby * _aby) * Math.Sqrt(_bcx * _bcx + _bcy * _bcy));
+        }
+
+        public Angle(double Ax, double Ay, double Bx, double By, double Cx, double Cy)
+        {
+            _ax = Ax;
+            _ay = Ay;
+            _bx = Bx;
+            _by = By;
+            _cx = Cx;
+            _cy = Cy;
+        }
+
+        public static bool operator >(Angle angle1, Angle angle2)
+        {
+            return angle1._angle > angle2._angle;
+        }
+
+        public static bool operator <(Angle angle1, Angle angle2)
+        {
+            return angle1._angle < angle2._angle;
+        }
+
+        public static bool operator ==(Angle angle1, Angle angle2)
+        {
+            return angle1._angle == angle2._angle;
+        }
+
+        public static bool operator !=(Angle angle1, Angle angle2) => throw new Exception("This operation cannot be performed.");
+
+        public static double operator +(Angle angle1, Angle angle2)
+        {
+            return angle1._angle + angle2._angle;
+        }
+
+        public static bool operator -(Angle angle1, Angle angle2) => throw new Exception("This operation cannot be performed.");
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+}
---
+++ Example/Example/ArrayScaling.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example
+{
+    public class ArrayScaling
+    {
+        private double _n;
+        private double[] _m;
+
+        public ArrayScaling(double N)
+        {
+            _n = N;
+        }
+
+        public ArrayScaling(double[] M)
+        {
+            _m = M;
+        }
+
+        public double[] Scaling(double[] array) //метод для масштабирования, зависимости от введеных при инициализации класса данных
+        {
+            if (_m.Length == 0) //значит, что при инициализации класса указали одно число N
+            {
+                for (var i = 0; i < array.Length; i++)
+                {
+                    array[i] *= _n;
+                }
+            }
+            else
+            {
+                for (var i = 0; i < array.Length; i++)
+                {
+                    array[i] *= _m[i];
+                }
+            }
+
+            return array;
+        }
+    }
+}
